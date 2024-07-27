1ed48f0945d615c080c32d22c40f86fea1d6cbdb
Elliot <elliotmichlin@gmail.com>
2024-07-27 19:43:36 -0400

    Updated mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   relative: true,
   content: ['./**/*.{html,js}', './index.html'],
   theme: {
+    screens: {
+      'xs': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
